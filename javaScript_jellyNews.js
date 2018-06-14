2f5bf4cbc0ee7e09d78fd5ce8c6774182b2b9a8f
Sharpe28 <36647107+Sharpe28@users.noreply.github.com>
2018-06-14 09:46:59 -0700

    Add files via upload

---
+++ javaScript_jellyNews.js
@@ -0,0 +1,16 @@
+window.alert("I can see the things you doin' And you think that I'm naive But when I get the goods on you She'll finally believe She says it's all just drama But every bubble's got to pop She's gonna see just what you're doin' And then you're finally gonna have to stop! Don't think you're gonna win this time Cause' you better believe I'm gonna drop a dime On you I'll get ya! Yeah! [2x] And when I do you're gonna be busted! Busted! I don't wanna put the hurt on you But you better believe me when I tell you That I finally got the dirt on you You're busted! (busted!) Yeah, she's finally gonna see your lie This is how it's gonna be When she finds out that I was always right!");
+
+function homicideFunction() {
+    document.getElementById("homicide").innerHTML = "On May 16th 2057, the big corporate Kraft was shut down due to illegal Jell-O production. According to Darylll S.Parker, a Swat officer whose squad had raided the Kraft factory, 'We infiltrated the factory around 11:29am, where we found the perpetrators using illegal ingredient 'gelatin'... there were about 48 workers. My squad was able to arrest 39 workers,2 of which were undocumented illegal citizens'. The CEO of Kraft will have a press conference on May 20th, where Ms. Rosenfeld is said to release an appology. Although, she has made it clear that she will not be answering any further questions.";
+    document.getElementById("titleStory").innerHTML = "The Craving For Jelly Still Exist?";
+}
+
+function camoflaugeFunction() {
+    document.getElementById("homicide").innerHTML = "The University of Transgression had discovered a new species, or evolution, of Jelly it has been named jellmeleon (Morone saxatilis). They scientist were sent on expedition to eastern Asia, where they initially were determined to study the local birds for any effects from polution, and they had stumbled upon a strange looking mango. to their astonishment, the mango was actually a jelly who was able to disguise itself as the fruit. Dr. Richtofen, a biology profesor, had told reporters, 'This new jelly species is an exciting transition, this species of jelly may become the new norm since as current research shows the previous species of jellies have had difficulty surviving the harsh environment. This new species of jellies may quite possibly be the biggest factor in the future generations of jellies, they might began an exponential growth. But who can say this is a definite, further research is required to make such giant claims.'the future for jellies is finally starting to look bright ever since the incident that brought a damper on jelly populations that we now call 'The Catalyzation'";
+    document.getElementById("titleStory").innerHTML = "A Bright Future for Jellies"
+}
+
+function rebelFunction() {
+    document.getElementById("homicide").innerHTML = "";
+    document.getElementById("titleStory").innerHTML = "thing"
+}
